6da8108e3a2fd69156faacfba0d77d12025d740d
Matvey Timofeev <mntimofeev07@gmail.com>
2025-10-13 15:30:34 +0500

    Улучшение Inc16Fast

--- 16bits.py
+++ 16bits.py
@@ -1,4 +1,4 @@
-for i in range(1, 15, 1):
+for i in range(15, 16, 1):
     print(f"""
 // -------------- i-{i} --------------
 Nand(a=in[{i}], b=curry{i-1}, out=N{i});
@@ -6,7 +6,7 @@ Nand(a=in[{i}], b=curry{i-1}, out=N{i});
 // sum (xor)
 Nand(a=in[{i}], b=N{i}, out=C1{i});
 Nand(a=curry{i-1}, b=N{i}, out=C2{i});
-Nand(a=C1{i}, b=C2{i}, out=out[{i})];
+Nand(a=C1{i}, b=C2{i}, out=out[{i}]);
 
 // carry (and)
 Nand(a=N{i}, b=N{i}, out=carry{i});
